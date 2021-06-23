e297de33ef75bfdee6fa54ddc8cff7fd4b069a04
Rick <dirklo@gmail.com>
2021-06-23 19:18:52 +0200

    add SVG logo

--- components/Landing.js
+++ components/Landing.js
@@ -3,10 +3,17 @@ import styles from '../styles/Landing.module.css'
 import portrait from '../public/portrait.png'
 import Image from 'next/image'
 import rickAvatar from '../public/rick-avatar.svg'
+import logo from '../public/SVGLogo.svg'
 
 export default function Landing() {
     return (
         <section className={styles.landing}>
+            <div className={styles.logo}>
+                <Image
+                src={logo}
+                alt='Logo'
+                ></Image>
+            </div>
             <div className={styles.name_div}>
                 <div className={styles.rect}></div>
                 <h1>Rick</h1>
