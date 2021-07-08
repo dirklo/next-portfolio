379b608caa693140b622715d147c8c6fdda3c733
Rick <dirklo@gmail.com>
2021-07-08 13:14:11 +0200

    convert landing back to module css

--- components/Landing.js
+++ components/Landing.js
@@ -3,7 +3,7 @@ import portrait from '../public/portrait.png'
 import Image from 'next/image'
 import SocialContainer from './SocialContainer'
 import { motion } from 'framer-motion'
-import styledJsx from '../styles/Landing.styles'
+import styles from '../styles/Landing.module.css'
 
 export default function Landing() {
     return (
@@ -11,10 +11,10 @@ export default function Landing() {
             initial={{ opacity: 0}}
             animate={{ opacity: 1}}
             exit={{ opacity: 0}}
-            className={`${styledJsx.className} landing`}
+            className={styles.landing}
         >
-            <div className={`${styledJsx.className} name_div`}>
-                <div className={`${styledJsx.className} rect`}></div>
+            <div className={styles.name_div}>
+                <div className={styles.rect}></div>
                 <motion.div 
                     initial="moved" 
                     animate="positioned"
@@ -30,7 +30,7 @@ export default function Landing() {
                         }
                     }}
                 >
-                    <h1 className={`${styledJsx.className} name_h1`}>Hey there, I&apos;m Rick!</h1>
+                    <h1>Hey there, I&apos;m Rick!</h1>
                 </motion.div>
                 <motion.div 
                     initial="moved" 
@@ -48,7 +48,7 @@ export default function Landing() {
                         }
                     }}
                 >    
-                    <h2 className={`${styledJsx.className} name_h2`}>I&apos;m a software engineer and web developer</h2>
+                    <h2>I&apos;m a software engineer and web developer</h2>
                 </motion.div>
                 <motion.div 
                     initial="moved" 
@@ -66,7 +66,7 @@ export default function Landing() {
                         }
                     }}
                 >    
-                    <div className={`${styledJsx.className} social_bar`}></div>
+                    <div className={styles.social_bar}></div>
                 </motion.div>
                 <motion.div 
                     initial="moved" 
@@ -88,7 +88,7 @@ export default function Landing() {
                 </motion.div>
             </div>
             <motion.div 
-                className={`${styledJsx.className} portrait_div`}
+                className={styles.portrait_div}
                 initial="moved" 
                 animate="positioned"
                 variants={{
@@ -104,7 +104,7 @@ export default function Landing() {
                 }}
             >
                 <Image 
-                    className={`${styledJsx.className} portrait`} 
+                    className={styles.portrait} 
                     src={portrait}
                     width={525}
                     height={700} 
@@ -114,7 +114,6 @@ export default function Landing() {
                     quality='50'
                 />
             </motion.div>
-            {styledJsx.styles}
         </motion.section>
     )
 }
