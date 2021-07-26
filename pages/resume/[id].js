7951b9287f0e9e164a9a681f9bbfd8e77280caba
Rick <dirklo@gmail.com>
2021-07-26 07:46:36 +0200

    refactor resume buttons

--- pages/resume/[id].js
+++ pages/resume/[id].js
@@ -18,6 +18,7 @@ export default function Resume() {
     const controls = useAnimation();
 
     const handleClick = (e) => {
+        console.log(e.target.dataset.id)
         router.push({
             pathname: `/resume/${e.target.dataset.id}`,
         }, undefined, { scroll: false })
@@ -36,8 +37,59 @@ export default function Resume() {
         }
     }
 
+    const buildResumeItemSelectButton = (title, icon) => {
+        const slug = title.toLowerCase()
+        const pickIcon = () => {
+            switch (icon) {
+                case 'GiTalk':
+                    return <GiTalk 
+                        color={router.query.id === 'testimonials' ? 'gold' : 'white'} 
+                        size='2em' 
+                        style={{ pointerEvents: 'none' }}
+                    />
+                case 'GiSkills':
+                    return <GiSkills 
+                        color={router.query.id === 'skills' ? 'gold' : 'white'} 
+                        size='2em' 
+                        style={{ pointerEvents: 'none' }}
+                    />
+                case 'CgWorkAlt':
+                    return <CgWorkAlt 
+                        color={router.query.id === 'experience' ? 'gold' : 'white'} 
+                        size='2em' 
+                        style={{ pointerEvents: 'none' }}    
+                    />
+                case 'FaGraduationCap':
+                    return <FaGraduationCap 
+                        color={router.query.id === 'education' ? 'gold' : 'white'} 
+                        size='2em' 
+                        style={{ pointerEvents: 'none' }}
+                    />
+            }
+        }
+        return (
+            <div
+                className={`${styledJsx.className} resume_button`}
+                onClick={(e) => handleClick(e)}
+                data-id={slug}
+            >
+                {pickIcon(icon)}
+                <span
+                    className={`${styledJsx.className} resume_button_span`}
+                    style={{ pointerEvents: 'none' }}
+                >
+                    {title}
+                </span>
+                <GoArrowRight 
+                    color='white' 
+                    size='2em' 
+                    style={{ pointerEvents: 'none' }}
+                />
+            </div>
+        )
+    }
+
     return (
-        
         <motion.div
             className={`${styledJsx.className} resume`}
             initial={{ opacity: 0 }}
@@ -117,94 +169,10 @@ export default function Resume() {
                 <div
                     className={`${styledJsx.className} resume_buttons`}
                 >
-                    <div
-                        className={`${styledJsx.className} resume_button`}
-                        onClick={(e) => handleClick(e)}
-                        data-id='testimonials'
-                    >
-                        <GiTalk 
-                            color={router.query.id === 'testimonials' ? 'gold' : 'white'} 
-                            size='2em' 
-                            style={{ pointerEvents: 'none' }}
-                        />
-                        <span
-                            className={`${styledJsx.className} resume_button_span`}
-                            style={{ pointerEvents: 'none' }}
-                        >
-                            Testimonials
-                        </span>
-                        <GoArrowRight 
-                            color='white' 
-                            size='2em' 
-                            style={{ pointerEvents: 'none' }}
-                        />
-                    </div>
-                    <div
-                        className={`${styledJsx.className} resume_button`}
-                        onClick={(e) => handleClick(e)}
-                        data-id='skills'
-                    >
-                        <GiSkills 
-                            color={router.query.id === 'skills' ? 'gold' : 'white'} 
-                            size='2em' 
-                            style={{ pointerEvents: 'none' }}
-                        />
-                        <span
-                            className={`${styledJsx.className} resume_button_span`}
-                            style={{ pointerEvents: 'none' }}
-                        >
-                            Skills
-                        </span>
-                        <GoArrowRight 
-                            color='white' 
-                            size='2em' 
-                            style={{ pointerEvents: 'none' }}
-                        />
-                    </div>
-                    <div
-                        className={`${styledJsx.className} resume_button`}
-                        onClick={(e) => handleClick(e)}
-                        data-id='experience'
-                    >
-                        <CgWorkAlt 
-                            color={router.query.id === 'experience' ? 'gold' : 'white'} 
-                            size='2em' 
-                            style={{ pointerEvents: 'none' }}    
-                        />
-                        <span
-                            className={`${styledJsx.className} resume_button_span`}
-                            style={{ pointerEvents: 'none' }}
-                        >
-                            Experience
-                        </span>
-                        <GoArrowRight 
-                            color='white' 
-                            size='2em' 
-                            style={{ pointerEvents: 'none' }}
-                        />
-                    </div>
-                    <div
-                        className={`${styledJsx.className} resume_button`}
-                        onClick={(e) => handleClick(e)}
-                        data-id='education'
-                    >
-                        <FaGraduationCap 
-                            color={router.query.id === 'education' ? 'gold' : 'white'} 
-                            size='2em' 
-                            style={{ pointerEvents: 'none' }}
-                        />
-                        <span
-                            className={`${styledJsx.className} resume_button_span`}
-                            style={{ pointerEvents: 'none' }}
-                        >
-                            Education
-                        </span>
-                        <GoArrowRight 
-                            color='white' 
-                            size='2em' 
-                            style={{ pointerEvents: 'none' }}
-                        />
-                    </div>
+                    {buildResumeItemSelectButton('Testimonials', 'GiTalk')}
+                    {buildResumeItemSelectButton('Skills', 'GiSkills')}
+                    {buildResumeItemSelectButton('Experience', 'CgWorkAlt')}
+                    {buildResumeItemSelectButton('Education', 'FaGraduationCap')}
                 </div>
             </div>
             <div className={`${styledJsx.className} right_pane`}>
