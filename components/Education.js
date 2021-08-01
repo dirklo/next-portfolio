4f99a595cc9a5db63a060cd691661d22d8a63db5
Rick <dirklo@gmail.com>
2021-08-01 16:15:27 +0200

    add education items

--- components/Education.js
+++ components/Education.js
@@ -9,7 +9,116 @@ export default function Education() {
             <div
                 className={`${styledJsx.className} education_card`}
             >
-                I AM AN EDUCATION
+                <h2
+                    className={`${styledJsx.className} education_card_h2`}
+                >
+                    Flatiron School 
+                </h2>
+                <h3
+                    className={`${styledJsx.className} education_card_h3`}
+                >
+                    Full Stack Software Engineering
+                </h3>
+                <div
+                    className={`${styledJsx.className} education_card_dates`}
+                >
+                    December 2020 - May 2021
+                </div>
+            </div>
+            <div
+                className={`${styledJsx.className} education_card`}
+            >
+                <h2
+                    className={`${styledJsx.className} education_card_h2`}
+                >
+                    Berklee College of Music    
+                </h2>
+                <h3
+                    className={`${styledJsx.className} education_card_h3`}
+                >
+                    Bachelors of Music, Music Production and Engineering
+                </h3>
+                <div
+                    className={`${styledJsx.className} education_card_dates`}
+                >
+                    August 2004 - May 2007
+                </div>
+            </div>
+            <div
+                className={`${styledJsx.className} education_card`}
+            >
+                <h2
+                    className={`${styledJsx.className} education_card_h2`}
+                >
+                    Udemy.com
+                </h2>
+                <h3
+                    className={`${styledJsx.className} education_card_h3`}
+                >
+                    JavaScript: The Advanced Concepts (2021)
+                </h3>
+                <div
+                    className={`${styledJsx.className} education_card_dates`}
+                >
+                    Completed April 2021
+                </div>
+            </div>
+            <div
+                className={`${styledJsx.className} education_card`}
+            >
+                <h2
+                    className={`${styledJsx.className} education_card_h2`}
+                >
+                    Udemy.com
+                </h2>
+                <h3
+                    className={`${styledJsx.className} education_card_h3`}
+                >
+                    The Web Developer Bootcamp (2020)
+                </h3>
+                <div
+                    className={`${styledJsx.className} education_card_dates`}
+                >
+                    Completed November 2021
+                </div>
+            </div>
+            <div
+                className={`${styledJsx.className} education_card`}
+            >
+                <h2
+                    className={`${styledJsx.className} education_card_h2`}
+                >
+                    Udemy.com
+                </h2>
+                <h3
+                    className={`${styledJsx.className} education_card_h3`}
+                >
+                    2020 Complete Python Bootcamp: From Zero to Hero in Python
+                </h3>
+                <div
+                    className={`${styledJsx.className} education_card_dates`}
+                >
+                    Completed November 2021
+                </div>
+            </div>
+            <div
+                className={`${styledJsx.className} education_card`}
+            >
+                <h2
+                    className={`${styledJsx.className} education_card_h2`}
+                >
+                    Udemy.com
+                </h2>
+                <h3
+                    className={`${styledJsx.className} education_card_h3`}
+                >
+                    The Complete SQL Bootcamp: From Zero to Hero
+                </h3>
+                <div
+                    className={`${styledJsx.className} education_card_dates`}
+                >
+                    Completed November 2021
+                </div>
             </div>
             {styledJsx.styles}  
         </div>
