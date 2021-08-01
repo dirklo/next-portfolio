8cead6b35387df15d3b3b433f46be0a59330c378
Rick <dirklo@gmail.com>
2021-08-01 15:31:54 +0200

    add animation staggering on skills

--- components/Skills.js
+++ components/Skills.js
@@ -1,16 +1,44 @@
 import React from 'react'
 import styledJsx from '../styles/Skills.styles'
 import { skills, pickIcon } from '../data/skillsData'
+import { motion } from 'framer-motion'
+import { delay } from 'q'
 
 export default function Skills() {
+    const buildVariants = (dir, delay) => {
+        return {
+            moved: dir === 'left' ? {x: -500, opacity: 0} : {x: 500, opacity: 0},
+            positioned: {
+                x: 0,
+                y: 0,
+                opacity: 1,
+                transition: {
+                    type: 'spring',
+                    stiffness: 20,
+                    delay: delay
+                }
+            }
+        } 
+    }
+
     return (
-        <div
+        <motion.div
             className={`${styledJsx.className} container`}
+            initial={{ opacity: 0 }}
+            animate={
+                { 
+                    opacity: 1
+                }
+            }
+
         >
             {skills.map((skill, index) =>
-                <div
+                <motion.div
                     key={index}
                     className={`${styledJsx.className} skill_card`}
+                    initial="moved" 
+                    animate="positioned"
+                    variants={buildVariants((index % 2 === 0 ? 'left' : 'right'), index * 0.05)}
                 >
                     <div
                         className={`${styledJsx.className} skill_card_logo`}
@@ -22,9 +50,9 @@ export default function Skills() {
                     >
                         {skill.name}
                     </h2>
-                </div>
+                </motion.div>
             )}
             {styledJsx.styles}
-        </div>
+        </motion.div>
     )
 }
